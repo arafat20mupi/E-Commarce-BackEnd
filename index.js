8cafcc7faf35970d38e24787d22d5363918f1eb2
tanjimN4 <tanjimahemed7@gmail.com>
2025-01-26 22:13:37 +0600

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ connectDB();
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     credentials: true,
   })
 );
