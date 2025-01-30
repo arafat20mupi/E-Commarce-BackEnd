fa5290482ec07044c191305d353a3e2cbdc4a391
tanjimN4 <tanjimahemed7@gmail.com>
2025-01-30 20:03:03 +0600

    add admin delete add get and update

--- Firebase/adminRouter.js
+++ Firebase/adminRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { allUsers, createUser, updateRole } = require('./adminController');
+const { allUsers, createUser, updateRole, deleteUser } = require('./adminController');
 const router = express.Router();
 
 router.get('/users',allUsers)
 router.patch('/update-role',updateRole)
+router.delete('/delete/user/:uid',deleteUser)
 
 module.exports = router
