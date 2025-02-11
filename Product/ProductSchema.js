351aa6d0361bda2bced4e792f5fe4943603859ab
tanjimN4 <tanjimahemed7@gmail.com>
2025-02-11 23:37:10 +0600

    fix reviewschma

--- Product/ProductSchema.js
+++ Product/ProductSchema.js
@@ -58,8 +58,8 @@ const ProductSchema = new mongoose.Schema(
                 name: {
                     type: String,
                 },
-                verified: {
-                    type: Boolean,
+                email:{
+                    type: String,
                 },
                 stars: {
                     type: Number,
